98ffa88d3fbea42f3af13db6bada44ee7b718cda
kev-kev <kevin.clark93@gmail.com>
2020-02-24 17:54:21 -0500

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,29 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
   end
 
+  def paintings
+    Painting.all.select{|painting|
+      painting.gallery == self
+    }
+  end
+
+  def artists
+    paintings.collect{|painting| painting.artist}
+  end
+
+  def artist_names
+    artists.collect{|artist| artist.name}
+  end
+
+  def most_expensive_painting
+    paintings.max_by{|painting| painting.price}
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,5 +1,17 @@
 require_relative '../config/environment.rb'
 
+picaso = Artist.new('pablo piaso', 40)
+davinci = Artist.new('leonardo davinci', 70)
+vangogh = Artist.new('vincent vangogh', 40)
+
+met = Gallery.new('met', 'nyc')
+louvre = Gallery.new('louvre', 'paris')
+npg = Gallery.new('national portait gallery', 'dc')
+
+le_reve = Painting.new('le reve', 1000000, picaso, met)
+mona_lisa = Painting.new('mona lisa', 11111, davinci, louvre)
+starry_night = Painting.new('starry night', 500000000, vangogh, npg)
+old_man = Painting.new('old man with guitar', 22222222, picaso, louvre)
 
 
 
